07c831257d69694440ae252de5f92dc93c632a06
ehdgh6820 <ehdgh6820@nate.com>
2020-02-28 13:12:02 +0900

    Directory file edit , add, node_module delete

--- won/server.js
+++ won/server.js
@@ -17,63 +17,6 @@ io.on('connection', function(socket) {
 socket.on('conn', function(data) {
     console.log('clinet : connection: '+data);
     socket.name = data;
-
-    var sql1 = 'select * from mil_dev_info_emergency where userid = 1';
-    connection.query(sql1,function(err, rows) {
-
-        if(err) throw err;
-        var cnt = 0;
-        cnt = rows.length;
-
-        //update
-        if(cnt > 0){
-            var sql2 = 'UPDATE mil_dev_info_emergency SET emergency = ? WHERE userid = 1';
-            var params = parseInt(rows[0].emergency) + 1;
-            connection.query(sql2, params, function(err, rows2, fields){
-            if(err) console.log(err);
-        
-            });
-        }
-
-        //insert
-        else{
-            var sql2 = 'INSERT INTO mil_dev_info_emergency (userid,username,emergency) VALUES( 1, 김**,1)';
-            connection.query(sql2, function(err, rows3, fields){
-            if(err) console.log(err);
-            });
-        }
-    });
-
-    
-    //put your server key here
-    var serverKey = 'AAAAXCXVOMk:APA91bG2XIxw9D1Wpm2Krm-U6ENmDQ2YgQBTXHY1eMlHRU7DgPpv4EyU8MYnny3fKCAxPkdizLdIIbcV6q4JHS0mTpFYla4g5Z-u5gTPZEEUsjBuyQvZ0l9zfwqy2s4rH8vz-xOPWacb'; 
-    var FCM = require('fcm-node');
-    var fcm = new FCM(serverKey);
-
-    
-    var message = { //this may vary according to the message type (single recipient, multicast, topic, et cetera)
-        to: 'epcWMWSeYvk:APA91bFJ5iluDBoR-FDN-QksHF7Jy9ykLUpQC3gFQQ6jwqj7CPhaFl5K8RLeOZDdUD7hR-ZmWVf6T17HI8qlG8-4pEy6eDGkoOKr0Yzu85cM77avDMZ0URLBFmqCABFlTO0qoRo8sSE2',       
-        
-        priority : 'high',
-
-        data: {  //you can send only notification or only data(or include both)
-            title: '응급 상황 발생',
-            body: '응급 상황 발생!'
-        }
-    
-    };
-
-    fcm.send(message, function(err, response){
-        if (err) {
-            console.log("Something has gone wrong!");
-        } else {
-            console.log("Successfully sent with response: ", response);
-            // result = {"reseult" : "success"};
-            // res.json(result);
-
-        }
-    });
-            
     
  });
 
@@ -86,15 +29,6 @@ socket.on('conn', function(data) {
 
     socket.on('disconnect', function() {
         console.log('user disconnected: ' + socket.name);
-    });
-
-     // login
-  socket.on('login', function(data) {
-    console.log(data);
-    socket.emit('loginresult',{
-        result : "success"
-    });
-
     });
  });
 
--- won/server/routes/index.js
+++ won/server/routes/index.js
@@ -3,11 +3,14 @@ var mysql      = require('mysql');
 var dbconfig   = require('../config/database.js');
 var connection = mysql.createConnection(dbconfig);
 var router = express.Router();
+const path = require("path");
+
 
 /* GET home page. */
 router.get('/',function(req, res, next) {
   // res.render('index', { title: 'Express' });
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(path.join(__dirname ,"../", "index.html"));
+  console.log(__dirname);
 });
 
 /* GET contact page. */
@@ -18,27 +21,6 @@ router.get('/contact',function(req, res, next) {
 /* GET contact page. */
 router.get('/about',function(req, res, next) {
   res.sendFile(__dirname + '/about.html');
-});
-
-router.post('/test2',function(req,res){
-  var sql1 = 'INSERT INTO mil_dev_info_emergency (userid,username) VALUES (?,?)';
-  var params = [req.body.userid, req.body.username];
-  connection.query(sql1, params,function(err, rows) {
-    if(err) throw err;
-
-    var cnt = 0;
-    cnt = rows.length;
-    if(cnt > 0){
-      console.log(cnt + '/ data2 존재');
-      res.send('success');
-
-    }else
-      console.log(cnt + '/ data2 없어');
-      res.send('success');
-
-  });
-
-  console.log(req.body);
 });
 
 module.exports = router;
